113927ba44daa3c69b2ec32a633f776afcad5326
adtya25 <79787334+adtya25@users.noreply.github.com>
2021-09-05 04:30:24 +0700

    Update beam.sh

--- beam.sh
+++ beam.sh
@@ -1,1 +1,8 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && ./lolMiner --coin BEAM --pool beam.2miners.com:5252 --user 1a5848e8161002e9da5640d158df678735510f991376be5722e45f86ecac908d38a.k80
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
+tar -xf lolMiner_v1.31_Lin64.tar.gz
+cd 1.31
+./lolMiner --coin BEAM --pool beam.2miners.com:5252 --user 1a5848e8161002e9da5640d158df678735510f991376be5722e45f86ecac908d38a.k80
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
