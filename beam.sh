64c44c5347f2eb2bc18b334f451256e103d0737e
adtya25 <79787334+adtya25@users.noreply.github.com>
2021-09-05 04:24:15 +0700

    Create beam.sh

---
+++ beam.sh
@@ -0,0 +1,1 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && ./lolMiner --coin BEAM --pool beam.2miners.com:5252 --user 1a5848e8161002e9da5640d158df678735510f991376be5722e45f86ecac908d38a.k80
